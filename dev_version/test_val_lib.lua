a8430a59b039fbb4ae6380c059df343b480f047f
Ulisse Mini <ulisse.mini@gmail.com>
2019-04-16 20:20:52 -0400

    Better tests

--- dev_version/test_val_lib.lua
+++ dev_version/test_val_lib.lua
@@ -1,4 +1,4 @@
--- Tester program used for making sure new versions work without bugs
+-- tester program used for making sure new versions work without bugs
 fs        = require("fs")
 turtle    = require("turtle")
 textutils = require("textutils")
@@ -8,33 +8,20 @@ local os   = require("os")
 local math = require("math")
 local h    = require("helpers")
 
-local startTime
-local defaultReturnValue = "OK"
+local defaultRet = "OK"
 local testFailures = 0
 
 local results = {}
-local Test = {}
+local test = {}
 
 t.debug_level = 3
 
 local function resetCoords()
   -- Reset the coordanites
-	t.goto(0,0,0,0)
+	t.moveTo(0,0,0,0)
 end
 
-local function checkResults(re)
-  for k, v in pairs(re) do
-    local msg = "Test."..k.." - "..v
-
-    if v ~= defaultReturnValue then
-      testFailures = testFailures + 1
-    end
-
-    print(msg)
-  end
-end
-
-function Test.inTable()
+function test.inTable()
   local testTable = {
     "foo",
     "bar",
@@ -43,22 +30,20 @@ function Test.inTable()
   }
 
   if t.inTable("poo poo pee pee man", testTable) then
-    return "t.inTable(\"poo poo pee pee man\") returned true expected false"
+    error "t.inTable(\"poo poo pee pee man\") errored true expected false"
   end
 
   if t.inTable("foo", testTable) == false then
-    return "t.inTable(\"foo\") returned false expected true"
+    error "t.inTable(\"foo\") errored false expected true"
   end
 
   if t.inTable(2019, testTable) == false then
-    return "t.inTable(2019) returned false expected true"
+    error "t.inTable(2019) errored false expected true"
   end
-
-  return defaultReturnValue
 end
 
-function Test.positions()
-  -- There is a *VERY* small chance of this test failing from every movement returning false.
+function test.positions()
+  -- There is a *VERY* small chance of this test failing from every movement erroring false.
   local startingPos = t.dumpCoords()
 
   t.saveCurrentPos("start")
@@ -75,26 +60,24 @@ function Test.positions()
     for i=1,5 do t.down() end
   end
 
-  -- Before you return to start make sure coordanites are not 0,0,0,0
+  -- Before you error to start make sure coordanites are not 0,0,0,0
   local middlePos = t.dumpCoords()
 
   -- If the coordanites are still 0, 0, 0, 0 then fail
   if textutils.serialize(middlePos) == textutils.serialize(startingPos) then
-    return "Coordanites did not change."
+    error "Coordanites did not change."
   end
   --print(textutils.serialize(middlePos))
 
-  t.gotoPos("start")
+  t.moveToPos("start")
   local endingPos = t.dumpCoords()
 
   if textutils.serialize(endingPos) ~= textutils.serialize(startingPos) then
-    return "Failed to return to start."
+    error "Failed to error to start."
   end
-
-  return defaultReturnValue
 end
 
-function Test.goto()
+function test.moveTo()
   local startingPos = t.dumpCoords()
   local expPos = {
     x = h.rand(-50, 50),
@@ -103,92 +86,87 @@ function Test.goto()
     orientation = h.rand(0, 3),
   }
 
-  t.goto(expPos.x, expPos.y, expPos.z, expPos.orientation)
+  t.moveTo(expPos.x, expPos.y, expPos.z, expPos.orientation)
 
   local endingPos = t.dumpCoords()
 
   if h.isEqual(endingPos, expPos) == false then
     print(textutils.serialize(endingPos))
-    return "Ending position is not equal to expected ending position"
+    error "Ending position is not equal to expected ending position"
   end
-
-  return defaultReturnValue
 end
 
-function Test.calcFuelForPos()
+function test.calcFuelForPos()
   resetCoords()
   local fuelNeeded
   t.saveCurrentPos("start")
 
   fuelNeeded = t.calcFuelForPos("start")
   if fuelNeeded ~= 0 then
-    return "fuelNeeded is "..tostring(fuelNeeded).." expected 0"
+    error("fuelNeeded is "..tostring(fuelNeeded).." expected 0")
   end
 
   local x, y, z = 30, -19, 49
   local o = t.orientation
 
   -- Move around some
-  t.goto(x,y,z,o)
+  t.moveTo(x,y,z,o)
 
   fuelNeeded = t.calcFuelForPos("start")
   if fuelNeeded ~= 98 then
-    return "fuelNeeded is "..tostring(fuelNeeded).." expected 98"
+    error("fuelNeeded is "..tostring(fuelNeeded).." expected 98")
   end
 
   -- Move some more
-  t.goto(-10,20,-30,0)
+  t.moveTo(-10,20,-30,0)
 
   fuelNeeded = t.calcFuelForPos("start")
   if fuelNeeded ~= 60 then
-    return "fuelNeeded is "..tostring(fuelNeeded).." expected 60"
+    error("fuelNeeded is "..tostring(fuelNeeded).." expected 60")
   end
-
-  return defaultReturnValue
 end
 
--- Test the turtle going backward correctly.
-function Test.back()
-end
-
-function Test.textutils()
-  local foo = {
+function test.textutils()
+  local want = {
     x = 32,
     y = 28,
   }
-  local expected = "{\n  y = 28,\n  x = 32,\n}"
 
   -- serialize then unserialize
-  local text = textutils.serialize(foo)
+  local text = textutils.serialize(want)
+  local got  = textutils.unserialize(text)
 
-  if text ~= expected then
-    return "textutils.serialize() did not return expected"
-  end
-
-  local bar = textutils.unserialize(text)
-
-  if bar.x ~= foo.x or bar.y ~= foo.y then
-    return "textutils.unserialize() failed"
-  end
-
-  return defaultReturnValue
-end
-
--- Run tests
-for testname, testcase in pairs(Test) do
-  startTime = os.clock()
-  result = testcase()
-  if result ~= nil then
-    results[testname] = result
+  if want.x ~= got.x then
+    error ('want x = %q; got x = %q'):format(want.x, got.x)
+  elseif want.y ~= got.y then
+    error ('want y = %q; got y = %q'):format(want.y, got.y)
   end
 end
 
-checkResults(results)
-print(string.format("\nRan "..table.size(Test).." Tests in %.5fs", os.clock() - startTime))
+
+local startTime = os.clock()
+
+local pad = 20
+-- Run the tests
+for tname, tc in pairs(test) do
+  local _, err = pcall(tc)
+
+  io.write(tname..(' '):rep(pad - #tname)..'| ')
+  if err then print(err)
+         else print 'PASSED'
+  end
+end
+
+print(("\nRan %d tests in %.5fs")
+  :format(table.size(test), os.clock() - startTime))
+
 if testFailures > 0 then
-  print(testFailures.." Test failures")
+  print(testFailures.." test failures")
 else
-  print("OK")
+  print "OK"
+
+  -- tests passed, cleanup files.
+  os.remove './val_lib.log'
+  os.remove './savedPositions'
+  os.remove './coords'
 end
--- cleanup
-resetCoords()
